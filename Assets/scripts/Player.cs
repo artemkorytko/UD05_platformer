24dec78c51dc052fed9e2d60c43082218d89bddf
Teistail <teistail@gmail.com>
2023-02-14 16:23:41 +0300

    add playerController

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,18 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Cinemachine;
+using DefaultNamespace;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float horizonttalSpeed = 5f;
+    [SerializeField] private float jumpImpulse = 5f;
+
+    private Rigidbody2D _rigidbody;
+    private bool _isActive = true;
+    private bool _isCanJump;
+
+    private float _maxVelocityMagnitude;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _maxVelocityMagnitude = Mathf.Sqrt(Mathf.Pow(jumpImpulse, 2f) + Mathf.Pow(horizonttalSpeed, 2));
+        var camera = FindObjectOfType<CinemachineVirtualCamera>();
+        camera.m_Follow = transform;
+        camera.m_LookAt = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if (!_isActive)
+            return;
         
+        Movement();
+        UpdateSide();
+    }
+    
+    private void Movement()
+    {
+        HorizontalMovement();
+        VerticalMovement();
+        ClampVelocity();
+    }
+    
+    private void HorizontalMovement()
+    {
+        var axis = SimpleInput.GetAxis("Horizontal");
+        var velocity = _rigidbody.velocity;
+        velocity.x = horizonttalSpeed * axis;
+        _rigidbody.velocity = velocity;
+    }
+    
+    private void VerticalMovement()
+    {
+        if (_isCanJump && SimpleInput.GetAxis("Vertical") > 0)
+        {
+            _isCanJump = false;
+            _rigidbody.AddForce(jumpImpulse * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+    
+    private void ClampVelocity()
+    {
+        var velocity = _rigidbody.velocity.magnitude;
+        velocity = Mathf.Clamp(velocity, 0, _maxVelocityMagnitude);
+        _rigidbody.velocity = _rigidbody.velocity.normalized * velocity;
+    }
+    
+    private void UpdateSide()
+    {
+         if (!(Mathf.Abs(_rigidbody.velocity.x) > 0))
+             return;
+         var sign = Mathf.Sign(_rigidbody.velocity.x);
+         var scale = Mathf.Sign(transform.localScale.x);
+         if (scale != sign)
+         {
+             var localScale = transform.localScale;
+             localScale.x *= -1;
+             transform.localScale = localScale;
+         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!_isActive)
+            return;
+        if (col.gameObject.GetComponentInChildren<Platform>())
+        {
+            _isCanJump = true;
+        }
+            
+            
     }
 }
